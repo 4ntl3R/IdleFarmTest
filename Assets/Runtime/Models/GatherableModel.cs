e02d5377d91e7c5df3fd9a7be08bb4ec45b46f6c
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2023-03-08 19:57:42 +0600

    Added gatherable model

---
+++ Assets/Runtime/Models/GatherableModel.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Runtime.Models
+{
+    public class GatherableModel
+    {
+        public event Action<GatherableModel> OnEmpty;
+        public event Action<GatherableModel> OnContaining;
+        
+        private readonly int _maxCapacity;
+        private int _currentCapacity;
+
+        public GatherableModel(int maxCapacity)
+        {
+            _maxCapacity = maxCapacity;
+            _currentCapacity = _maxCapacity;
+        }
+
+        public void Use()
+        {
+            if (_currentCapacity-- <= 0)
+            {
+                OnEmpty?.Invoke(this);
+                return;
+            }
+            OnContaining?.Invoke(this);
+        }
+
+        public void Recharge()
+        {
+            _currentCapacity = _maxCapacity;
+        }
+    }
+}
