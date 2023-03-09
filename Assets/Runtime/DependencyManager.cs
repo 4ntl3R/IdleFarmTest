f573ae6c4d34298995971901ad928bc18f82eb82
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2023-03-10 00:01:23 +0600

    Update DependencyManager.cs
    Added resource classes

--- Assets/Runtime/DependencyManager.cs
+++ Assets/Runtime/DependencyManager.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using AKhvalov.IdleFarm.Runtime.Controllers;
 using AKhvalov.IdleFarm.Runtime.Data;
+using AKhvalov.IdleFarm.Runtime.Models;
 using AKhvalov.IdleFarm.Runtime.Pool;
 using AKhvalov.IdleFarm.Runtime.Views;
+using DG.Tweening;
 using UnityEngine;
 
 namespace AKhvalov.IdleFarm.Runtime
@@ -25,16 +27,26 @@ namespace AKhvalov.IdleFarm.Runtime
         private int gatherableCapacity = 1;
 
         [SerializeField] 
-        private List<InteractionReactorView> _gatherables;
+        private int lootCapacity = 40;
 
         [SerializeField] 
-        private AnimationData _animationData;
+        private int lootCost = 1;
 
+        [SerializeField] 
+        private List<InteractionReactorView> gatherables;
+
+        [SerializeField] 
+        private AnimationData animationData;
+
+        [SerializeField] 
+        private ResourcesView resourcesView;
+        
+        
         private GameObjectPool _lootPool;
-
         private GatherableController _gatherableController;
-
         private PlayerMovementController _playerMovementController;
+        private ResourcesModel _resourcesModel;
+        private ResourcesController _resourcesController;
         
         
         private void Start()
@@ -51,13 +63,16 @@ namespace AKhvalov.IdleFarm.Runtime
         private void CreateClasses()
         {
             _lootPool = new GameObjectPool(lootPrefab);
-            _gatherableController = new GatherableController(_lootPool, _gatherables, gatherableCapacity, _animationData.GrowParameters);
+            _gatherableController = new GatherableController(_lootPool, gatherables, gatherableCapacity, animationData.GrowParameters);
             _playerMovementController = new PlayerMovementController(inputJoystickController, playerMovementView);
+            _resourcesModel = new ResourcesModel(lootCapacity, lootCost);
+            _resourcesController = new ResourcesController(resourcesView, _resourcesModel, _lootPool);
         }
 
         private void ManageInjections()
         {
             playerMovementView.Inject(playerSpeedMultiplier);
+            resourcesView.Inject(lootCapacity);
         }
 
         private void DeleteSubscriptions()
@@ -65,6 +80,7 @@ namespace AKhvalov.IdleFarm.Runtime
             _lootPool.UnsubscribeEvents();
             _gatherableController.UnsubscribeEvents();
             _playerMovementController.UnsubscribeEvents();
+            _resourcesController.UnsubscribeEvents();
         }
     }
 }
