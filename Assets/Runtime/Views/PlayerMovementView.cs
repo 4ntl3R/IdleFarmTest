307924707f387b6bf02f80ede78ad2dd995bb309
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2023-03-13 17:01:00 +0600

    Update PlayerMovementView.cs
    Added character rotation

--- Assets/Runtime/Views/PlayerMovementView.cs
+++ Assets/Runtime/Views/PlayerMovementView.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 namespace AKhvalov.IdleFarm.Runtime.Controllers
 {
     public class PlayerMovementView : MonoBehaviour
     {
+        private static float magnitudeDeadZone = 0.01f;
+        
         private float _speedMultiplier;
         
         private Rigidbody _rigidbody;
@@ -16,8 +19,15 @@ namespace AKhvalov.IdleFarm.Runtime.Controllers
 
         public void ChangeVelocity(Vector2 value)
         {
-            var velocityVector = new Vector3(value.x * _speedMultiplier, 0, value.y * _speedMultiplier);
-            _rigidbody.velocity = velocityVector;
+            if (Math.Abs(value.magnitude) < magnitudeDeadZone)
+            {
+                _rigidbody.velocity = Vector3.zero;
+                return;
+            }
+            
+            var eulerAngle = Vector2.SignedAngle(Vector2.up, new Vector2(-value.x, value.y));
+            _rigidbody.MoveRotation(Quaternion.Euler(0, eulerAngle, 0));
+            _rigidbody.velocity = _speedMultiplier * value.magnitude * new Vector3(value.x, 0, value.y);
         }
     }
 }
